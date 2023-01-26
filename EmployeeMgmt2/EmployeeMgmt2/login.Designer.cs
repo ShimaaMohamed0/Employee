3a7e76ce6945d7e9d3b31495cca3581519f81a6f
ShimaaMohamed0 <118725184+ShimaaMohamed0@users.noreply.github.com>
2023-01-26 19:08:25 +0200

    add color tp panel

--- EmployeeMgmt2/EmployeeMgmt2/login.Designer.cs
+++ EmployeeMgmt2/EmployeeMgmt2/login.Designer.cs
@@ -33,6 +33,7 @@
             // 
             // flowLayoutPanel1
             // 
+            this.flowLayoutPanel1.BackColor = System.Drawing.Color.Teal;
             this.flowLayoutPanel1.Location = new System.Drawing.Point(51, 52);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
             this.flowLayoutPanel1.Size = new System.Drawing.Size(279, 67);
