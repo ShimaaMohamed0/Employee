840d4366d4300ab412c64cf07f97fd45550d9115
ShimaaMohamed0 <118725184+ShimaaMohamed0@users.noreply.github.com>
2023-01-26 19:04:59 +0200

    change border style

--- EmployeeMgmt2/EmployeeMgmt2/login.Designer.cs
+++ EmployeeMgmt2/EmployeeMgmt2/login.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace EmployeeMgmt2
 {
-    partial class Form1
+    partial class login
     {
         /// <summary>
         /// Required designer variable.
@@ -28,10 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(621, 300);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "login";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
