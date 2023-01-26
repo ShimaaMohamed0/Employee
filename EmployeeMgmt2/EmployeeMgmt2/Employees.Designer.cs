6dfe6525382f1082abf1505636c03e8ae7944a44
ShimaaMohamed0 <118725184+ShimaaMohamed0@users.noreply.github.com>
2023-01-26 20:12:46 +0200

    change font color to another label

--- EmployeeMgmt2/EmployeeMgmt2/Employees.Designer.cs
+++ EmployeeMgmt2/EmployeeMgmt2/Employees.Designer.cs
@@ -57,7 +57,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.ForeColor = System.Drawing.Color.Teal;
             this.label1.Location = new System.Drawing.Point(462, 343);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(356, 40);
