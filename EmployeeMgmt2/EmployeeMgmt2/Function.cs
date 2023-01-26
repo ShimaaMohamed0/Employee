5eaf56340435ef017b1de1d21d7f271b6941a4c8
ShimaaMohamed0 <118725184+ShimaaMohamed0@users.noreply.github.com>
2023-01-27 00:18:53 +0200

    add public function

--- EmployeeMgmt2/EmployeeMgmt2/Function.cs
+++ EmployeeMgmt2/EmployeeMgmt2/Function.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,14 @@ namespace EmployeeMgmt2
 {
     internal class Function
     {
+        private SqlConnection con;
+        private SqlCommand cmd;
+        private DataTable dt;
+        private SqlDataAdapter sda;
+        private string constr;
+        public Function()
+        {
+
+        }
     }
 }
